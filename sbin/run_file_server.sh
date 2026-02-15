3fa457b42c42694accb058f88207e184892a8684
PK <pro.kamath@gmail.com>
2026-02-15 18:28:41 +0530

    Refactor file server startup command for improved readability and log output path update to logs/file_server.log.

--- sbin/run_file_server.sh
+++ sbin/run_file_server.sh
@@ -3,4 +3,7 @@
 source .venv/bin/activate
 source .env
 
-python -m uvicorn autobots_devtools_shared_lib.common.servers.fileserver.app:app --reload --host 0.0.0.0 --port ${FILE_SERVER_PORT:-9002} 2>&1 | tee -a file_server.log
+python -m uvicorn autobots_devtools_shared_lib.common.servers.fileserver.app:app \
+    --reload \
+    --host 0.0.0.0 \
+    --port ${FILE_SERVER_PORT:-9002} 2>&1 | tee -a logs/file_server.log
